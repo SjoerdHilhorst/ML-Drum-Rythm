de5ab856d7e0aa0f5cf90ff3ddf08b48ef7f730b
Lonnekee <lonneke.pulles@gmail.com>
2024-02-01 17:51:51 +0100

    Add combined decision making algorithm

--- decision_making.py
+++ decision_making.py
@@ -23,10 +23,16 @@ def graph(data, filename):
 
 
 def threshold_signal(data, threshold=0.5):
+    """
+    Threshold a matrix of values to a binary matrix, given a threshold.
+    """
     return (data > threshold).astype(int)
 
 
-def probability_signal(probabilities):    
+def probability_signal(probabilities):
+    """
+    Draw a binary matrix from a matrix of Bernoulli probabilities.
+    """
     # Generate a uniform random matrix of the same shape as the input data
     random_matrix = np.random.rand(*probabilities.shape)
     
@@ -41,16 +47,28 @@ def sigmoid(x):
 
 
 def sigmoid_signal(data, scaling_factor):
+    """
+    Transform a matrix of values to a matrix of Bernoulli probabilities
+    using a sigmoid function.
+    """
     return sigmoid(scaling_factor*(data - 0.5))
 
 
+def combined_decision_algorithm(probabilities, scaling_factor):
+    """
+    Combine the sigmoid signal and probability signal algorithms.
+    """
+    transformed_probabilities = sigmoid_signal(probabilities, scaling_factor)
+    return probability_signal(transformed_probabilities)
+
+
 initial_signal = np.random.rand(3, 16)
-graph(initial_signal, "img/initial_signal.png")
+graph(initial_signal, "img/linear_regression/thresholding/initial_signal.png")
 treshholded_signal = threshold_signal(initial_signal)
-graph(treshholded_signal, "img/treshholded_signal.png")
+graph(treshholded_signal, "img/linear_regression/thresholding/treshholded_signal.png")
 diced_signal = probability_signal(initial_signal)
-graph(diced_signal, "img/probability_signal.png")
+graph(diced_signal, "img/linear_regression/thresholding/probability_signal.png")
 transformed_signal = sigmoid_signal(initial_signal, 8)
-graph(transformed_signal, "img/transformed_signal.png")
+graph(transformed_signal, "img/linear_regression/thresholding/transformed_signal.png")
 diced_transformed_signal = probability_signal(transformed_signal)
-graph(diced_transformed_signal, "img/diced_transformed_signal.png")
+graph(diced_transformed_signal, "img/linear_regression/thresholding/diced_transformed_signal.png")
